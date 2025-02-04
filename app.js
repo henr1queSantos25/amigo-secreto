7cfb916b330b4a05d5f939f0c9f5082a578d2245
Henrique O. Santos <202410486@uesb.edu.br>
2025-02-04 12:04:56 -0300

    função que insere a lista na tela

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ function clearTextField(){
     document.querySelector('input').value = '';
 }
 
+function atualizarLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = ''; // Limpa a lista antes de recriá-la
+
+    listaAmigos.forEach(amigo => {
+        let li = document.createElement("li"); // Cria um <li>
+        li.textContent = amigo; // Adiciona o nome do amigo como texto
+        ul.appendChild(li); // Adiciona o <li> dentro do <ul>
+    });
+}
+
 function adicionarAmigo(){
     if(document.querySelector('input').value == ''){
         return 0;
@@ -12,4 +23,5 @@ function adicionarAmigo(){
     listaAmigos.push(amigo);
     console.log(listaAmigos);
     clearTextField();
+    atualizarLista();
 }
